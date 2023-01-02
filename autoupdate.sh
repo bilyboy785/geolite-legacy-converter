77c7ae177a8bec98bf5ab9900af12abd47b494ae
Martin Bouillaud <contact@bouillaudmartin.fr>
2023-01-02 22:42:23 +0100

    Update autoupdate script

--- autoupdate.sh
+++ autoupdate.sh
@@ -4,12 +4,12 @@ COUNTRY_IPV4="https://dl.miyuru.lk/geoip/maxmind/country/maxmind4.dat.gz"
 COUNTRY_IPV6="https://dl.miyuru.lk/geoip/maxmind/country/maxmind6.dat.gz"
 CITY_IPV4="https://dl.miyuru.lk/geoip/maxmind/city/maxmind4.dat.gz"
 CITY_IPV6="https://dl.miyuru.lk/geoip/maxmind/city/maxmind6.dat.gz"
-DEFAULT_PATH="./GeoLite"
+DEFAULT_PATH="/etc/nginx/geoip"
 
 if [[ ! -z $1 ]]; then
     OUTPUT_PATH=$1
 else
-    read -p "Full Path to store dat files : (default : ./GeoLite) " OUTPUT_PATH
+    read -p "Full Path to store dat files : (default : /etc/nginx/geoip) " OUTPUT_PATH
     OUTPUT_PATH="${OUTPUT_PATH:=${DEFAULT_PATH}}"
 fi
 
@@ -19,12 +19,23 @@ if [[ ! -d ${OUTPUT_PATH} ]]; then
 fi
 
 echo "Downloading GZ files"
-wget -q -O ${OUTPUT_PATH}/GeoLite-Country-IPV4.dat.gz ${COUNTRY_IPV4}
-wget -q -O ${OUTPUT_PATH}/GeoLite-Country-IPV6.dat.gz ${COUNTRY_IPV6}
-wget -q -O ${OUTPUT_PATH}/GeoLite-City-IPV4.dat.gz ${CITY_IPV4}
-wget -q -O ${OUTPUT_PATH}/GeoLite-City-IPV6.dat.gz ${CITY_IPV6}
+case $2 in 
+    ipv4)
+        wget -q -O ${OUTPUT_PATH}/geolite-country-ipv4.dat.gz ${COUNTRY_IPV4}
+        wget -q -O ${OUTPUT_PATH}/geolite-city-ipv4.dat.gz ${CITY_IPV4}
+        ;;
+    ipv6)
+        wget -q -O ${OUTPUT_PATH}/geolite-country-ipv6.dat.gz ${COUNTRY_IPV6}
+        wget -q -O ${OUTPUT_PATH}/geolite-city-ipv6.dat.gz ${CITY_IPV6}
+        ;;
+    *)
+        wget -q -O ${OUTPUT_PATH}/geolite-country-ipv6.dat.gz ${COUNTRY_IPV6}
+        wget -q -O ${OUTPUT_PATH}/geolite-city-ipv6.dat.gz ${CITY_IPV6}
+        wget -q -O ${OUTPUT_PATH}/geolite-country-ipv4.dat.gz ${COUNTRY_IPV4}
+        wget -q -O ${OUTPUT_PATH}/geolite-city-ipv4.dat.gz ${CITY_IPV4}
+        ;;
+esac
 
-echo "Extracting GZ files"
 for GZIP_FILE in $(find ${OUTPUT_PATH} -type f -name "*.gz")
 do
     gunzip $GZIP_FILE
